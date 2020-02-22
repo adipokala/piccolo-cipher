3f3a715bf1da230aea38bff766ec0f9dda6f29b9
adipokala <adithyapokala@outlook.com>
2020-02-22 17:17:16 +0100

    key generation

---
+++ piccolo-cipher/piccolo.py
@@ -0,0 +1,16 @@
+import piccolokeygen
+
+# class Piccolo:
+#     def __init__(self, key):
+#         self.key = key
+
+#     def encrypt(self, string):
+#         estring = string
+#         return estring
+
+#     def decrypt(self, estring):
+#         string = estring
+#         return string
+
+print(piccolokeygen.piccolokeygen(80))
+print(piccolokeygen.piccolokeygen(128))
